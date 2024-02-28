864d70a999589255619ee0c0f68ef671d391f418
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-02-28 18:06:46 +0100

    FINITO

--- script.js
+++ script.js
@@ -2,27 +2,21 @@
 //////////////Palindroma/////////////////
 /////////////////////////////////////////
 
-
 //Chiedo all'utente di inserire una parola minuscola
 let parolaUser = prompt("Per favore inserisci una parola palindroma scritta in minuscolo.")
 
 // Funzione per verificare se è palindroma alla stringa
 
-
 function parolaPalindroma(stringa) {
   //Divido la parola
   for (let i = 0; i < stringa.length / 2; i++) {
     //Controllo il contenuto se sbagliato
-    if (stringa[i] !== stringa[stringa.length - 1 - i]) {
-      
+    if (stringa[i] !== stringa[stringa.length - 1 - i]) {    
       return false;
     }
-  }
-  
+  }  
   return true;
 }
-
-
 
 //
 if (parolaUser !== null && parolaUser === parolaUser.toLowerCase()) {
@@ -45,6 +39,9 @@ let numGiocatore;
 let validPariDispari = false;
 let validNumber = false;
 
+let numeroPc = numeroCasuale (1,5);
+
+
 // fino a quando non è valido l'iserimento di pari/dispari ripropongo il prompt
 while (!validPariDispari) {
   pariDispari = prompt('Scrivi "pari" oppure "dispari"')
@@ -57,22 +54,28 @@ while (!validPariDispari) {
   }
 }
 
-while(!validNumber){
-  numGiocatore = parseInt(prompt('Scrivi un numero da 1 a 5'));
+
+numGiocatore = parseInt(prompt('Scrivi un numero da 1 a 5'));
   if(isNaN(numGiocatore) === false){
-    if(numGiocatore > 0 && numGiocatore <=5){
-      validNumber = true;
+  if(numGiocatore > 0 && numGiocatore <=5){
+    validNumber = true;
     }else{
       alert('Attenzione! hai inserito un numero sbagliato')
     }
   }else{
     alert('Attenzione! hai inserito una stringa e non un numero')
   }
-}
 
 
+  let sommaNumRisultato = numGiocatore + numeroPc
 
-
+  if (pariDispari === numPari(sommaNumRisultato)) {
+    alert ("hai vinto")
+  } else{
+    alert ("hai perso")
+  }
+  
+  console.log (parolaUser, numGiocatore, pariDispari, numeroPc, sommaNumRisultato)
 ////////////////////////////////////////////////
 ///////////////////Funzioni/////////////////////
 ////////////////////////////////////////////////
@@ -84,7 +87,11 @@ function numeroCasuale(min,max) {
 
 ///Funzione per vedere se un numero è pari o dispari
 function numPari(num) {
-  return num % 2 === 0; 
+  if (num % 2 === 0){
+    return "pari"
+  } else {
+    return "dispari"
+  }
 }
 
 
